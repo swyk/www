f39af6f1c915a2c9b833fc21d21dc79b567d653d
Madhu Kumar Dadi <mkdadi@outlook.in>
2016-07-12 09:20:36 +0530

    Modified

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='SWYK',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='SWYK',
+      author='Madhu Kumar',
+      author_email='kumardadi@outlook.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Django=1.8.5'],
+      install_requires=['Django==1.8.5'],
      )
