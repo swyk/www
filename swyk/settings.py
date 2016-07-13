6db96386d3c1c4d0f3477d613c4c5d2cae8fe307
Madhu Kumar Dadi <mkdadi@outlook.in>
2016-07-13 10:57:45 +0530

    Static Files added

--- swyk/settings.py
+++ swyk/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticb')
+STATIC_ROOT = os.path.join(BASE_DIR,'wsgi','static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
