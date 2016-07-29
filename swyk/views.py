c43edfcacd9378073cbf0563a090c3b49b6fbe15
Madhu Kumar Dadi <mkdadi@outlook.in>
2016-07-29 20:17:25 +0530

    Mail

--- swyk/views.py
+++ swyk/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from comingSoon.models import *
 
+import smtplib
+
 
 def ajax(request,offset):
 	return render(request,offset)
@@ -18,6 +20,24 @@ def home(request):
 		newSubscriber = Subscriber()
 		newSubscriber.email = request.POST['email']
 		newSubscriber.save()
+		
+		sender = 'team@swyk.cf'
+		receivers = [request.POST['email']]
+		
+		message = """From: SWYK Team <team@swyk.cf>\nTo: %s\nReply-To: no-reply@swyk.cf\nMIME-Version: 1.0\nContent-type: text/html\nSubject: Share What You Know Subscription\n\n
+			
+			Hi there,<br/>
+			Thank you for subscribing to <i><b>Share What You Know</b></i>, you will be the first to know about
+			any updates for the website.<br/><br/>
+			
+			If you didn't regester for SWYK, you can unsubscribe through our next email.<br/><br/>
+			
+			Best,<br/>
+			SWYK Team.<br/>
+			"""%(",".join(receivers))
+		smtpObj = smtplib.SMTP_SSL('smtp.yandex.com')
+		smtpObj.login("team@swyk.cf","DYKWIA@bcf184")
+		smtpObj.sendmail(sender, receivers, message)
 		
 		context['message'] = 1
 	return render(request,"comingSoon.html",context)
